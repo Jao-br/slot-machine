75c8697db0ef1c93c648a342b68850127e1a052b
JOÃO CARLOS CASTRO MENDES DE LIMA <joao.clima@a.ucb.br>
2023-11-06 21:54:19 -0300

    Add get_slot_machine_spin

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 # slot-machine
+import random
 
 MAX_LINES = 3
 MAX_BET = 1000
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.itens():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+
 
 
 def deposit():
@@ -49,10 +81,17 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    total_bet = bet * lines
+    
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
 
-    print(f"You are betting ${bet} no {lines}. Total bet is equal to: ${total_bet}")
+        if total_bet > balance:
+            print(f"Your current balance is insuficient to complete that bet. Current balance: ${balance}")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
     print(balance, lines)   
 
 main()
